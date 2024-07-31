bd6e7e459d39180ab0c44e40004815d9280beb6d
AwAjie <139576615+ajdgg@users.noreply.github.com>
2024-07-31 13:38:21 +0800

    更新

--- schedulers/weekly_rss.py
+++ schedulers/weekly_rss.py
@@ -5,24 +5,22 @@ from core.scheduler import CronTrigger, Scheduler
 from modules.weekly import get_weekly
 from modules.weekly.teahouse import get_rss as get_teahouse_rss
 from modules.weekly.ysarchives import get_rss as get_ysarchives_rss
-from datetime import datetime, timedelta
-from apscheduler.triggers.interval import IntervalTrigger
+from datetime import datetime
+
 
 # 尝试更新装饰器触发逻辑
-def calculate_this_sunday():
+def is_even_week():
     today = datetime.now()
-    days_until_sunday = (6 - today.weekday()) % 7
-    return today + timedelta(days=days_until_sunday)
-
-this_sunday = calculate_this_sunday().replace(hour=19, minute=10)
+    return today.isocalendar()[1] % 2 == 0
 
 
-@Scheduler.scheduled_job(trigger=IntervalTrigger(weeks=2), next_run_time = this_sunday)
+@Scheduler.scheduled_job(trigger=CronTrigger.from_crontab('10 19 * * SUN'))
 async def weekly_rss():
     Logger.info('Checking ysarchives biweekly...')
 
-    weekly = await get_ysarchives_rss()
-    await JobQueue.trigger_hook_all('ysarchives_weekly_rss', weekly=weekly)
+    if is_even_week():
+        weekly = await get_ysarchives_rss()
+        await JobQueue.trigger_hook_all('ysarchives_weekly_rss', weekly=weekly)
 
 
 @Scheduler.scheduled_job(CronTrigger.from_crontab('0 9 * * MON'))
