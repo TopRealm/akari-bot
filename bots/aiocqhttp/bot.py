a4af90f07b65f7f3b6d74a3ec8aa159d468f5c30
多羅狼 <dorowolf314@gmail.com>
2024-10-06 22:12:12 +0800

    u

--- bots/aiocqhttp/bot.py
+++ bots/aiocqhttp/bot.py
@@ -53,15 +53,14 @@ async def message_handler(event: Event):
         string_post = True
 
     if string_post:
-        match_json = re.match(r'.*?\[CQ:json,data=(.*?)\].*?', event.message, re.MULTILINE | re.DOTALL)
+        match_json = re.match(r'\[CQ:json,data=(.*?)\]', event.message, re.MULTILINE | re.DOTALL)
         if match_json:
             load_json = json.loads(html.unescape(match_json.group(1)))
             if load_json['app'] == 'com.tencent.multimsg':
                 event.message = f'[CQ:forward,id={load_json["meta"]["detail"]["resid"]}]'
     else:
-        match_json = event.message[0]["type"] == "json"
-        if match_json:
-            load_json = json.loads(html.unescape(event.message[0]["data"]["data"]))
+        if event.message[0]["type"] == "json":
+            load_json = json.loads(event.message[0]["data"]["data"])
             if load_json['app'] == 'com.tencent.multimsg':
                 event.message = [{"type": "forward", "data": {"id": f"{load_json["meta"]["detail"]["resid"]}"}}]
 
