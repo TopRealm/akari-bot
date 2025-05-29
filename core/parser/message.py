52f5f2157ddd1d67ffcf921838d88dbad838da2a
多羅狼 <Dorowolf314@gmail.com>
2025-05-29 17:37:48 +0800

    update

--- core/parser/message.py
+++ core/parser/message.py
@@ -488,7 +488,7 @@ async def _check_temp_ban(msg: Bot.MessageSession):
                 is_temp_banned["count"] += 1
                 await msg.finish(I18NContext("tos.message.tempbanned.warning", ban_time=int(TOS_TEMPBAN_TIME - ban_time)))
             else:
-                raise AbuseWarning("{tos.message.reason.ignore}")
+                raise AbuseWarning("{I18N:tos.message.reason.ignore}")
 
 
 async def _tos_msg_counter(msg: Bot.MessageSession, command: str):
@@ -499,16 +499,16 @@ async def _tos_msg_counter(msg: Bot.MessageSession, command: str):
                                               "ts": datetime.now().timestamp()}
     else:
         same["count"] += 1
-        if same["count"] > 5:
-            raise AbuseWarning("{tos.message.reason.cooldown}")
+        if same["count"] > 10:
+            raise AbuseWarning("{I18N:tos.message.reason.cooldown}")
     all_ = counter_all.get(msg.target.sender_id)
     if not all_ or datetime.now().timestamp() - all_["ts"] > 300:  # 检查是否滥用（5分钟内使用20条命令）
         counter_all[msg.target.sender_id] = {"count": 1,
                                              "ts": datetime.now().timestamp()}
     else:
         all_["count"] += 1
-        if all_["count"] > 10:
-            raise AbuseWarning("{tos.message.reason.abuse}")
+        if all_["count"] > 20:
+            raise AbuseWarning("{I18N:tos.message.reason.abuse}")
 
 
 async def _execute_submodule(msg: Bot.MessageSession, module, command_first_word):
