a12d0fc1b812b6be1430e93e2a6b0f47fa891250
RegadPoleCN <bilibilipopojun@hotmail.com>
2025-06-10 20:56:42 +0800

    fix async

--- modules/yunhei/user.py
+++ modules/yunhei/user.py
@@ -101,11 +101,11 @@ async def check(msg: Bot.MessageSession, qqnum: str = "all"):
                 light = moderate = severe = 0
                 tasks = []
                 if (l := len(group_members)) <= 50:
-                    tasks.append(_check_yunhei_api(group_members))
+                    tasks.append(asyncio.create_task(_check_yunhei_api(group_members)))
                 else:
                     member_sub_arrays = [group_members[i:i + (l//4)] for i in range(0, l, (l//4))]
                     for arr in member_sub_arrays:
-                        tasks.append(_check_yunhei_api(arr))
+                        tasks.append(asyncio.create_task(_check_yunhei_api(arr)))
                 done, pending = await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
                 for task in done:
                     for user_info in task.result():
