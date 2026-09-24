f8116980a37674b925a1398c4b0cd5ffe4cfc732
ZoruaFox <96456728+ZoruaFox@users.noreply.github.com>
2026-09-24 23:35:09 +0800

    Refactor get_rss to use feedparser and get_url

--- modules/weekly/teahouse.py
+++ modules/weekly/teahouse.py
@@ -1,11 +1,21 @@
-from .rss_utils import fetch_latest_entry
+import feedparser
+
+from core.utils.html2text import html2text
+from core.utils.http import get_url
 
 
 async def get_rss():
-    """
-    Get RSS feed from lakeus.xyz
-    """
-    return await fetch_latest_entry(
+    """Get RSS feed from lakeus.xyz"""
+    url = await get_url(
         "https://lakeus.xyz/api.php?action=featuredfeed&feed=teahouse-weekly&feedformat=atom",
-        "https://lakeus.xyz",
+        status_code=200,
+        fmt="text",
     )
+    feed_data = feedparser.parse(url)
+    entries = feed_data.get("entries", [])
+    if not entries:
+        return ""
+    feed = entries[-1]
+    title = feed.get("title", "")
+    summary = html2text(feed.get("summary", ""), baseurl="https://lakeus.xyz")
+    return title + "\n" + summary
